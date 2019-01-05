bbf4c066ce1b31c464f6d2a5e5e75dd4c1b8e118
denibulkashvili <denibulkashvili@gmail.com>
2019-01-06 05:29:02 +0700

    Create todoitem model

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class ToDoItem(models.Model):
+	title = models.CharField(max_length=100)
+	description = models.CharField(max_length=1000)
+	completed = models.BooleanField()
+	created_at = models.DateTimeField(auto_now_add=True)
